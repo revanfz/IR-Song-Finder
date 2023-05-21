b990a76f463c3f64eb7c2dd89ccf57157eb25dd1
BilhaqAD07 <87814270+BilhaqAD07@users.noreply.github.com>
2023-05-21 15:53:06 +0700

    Updating error handling

--- main.py
+++ main.py
@@ -2,9 +2,10 @@
 
 import pandas as pd
 import numpy as np
-
+from tkinter import messagebox
 from tkinter import *
 from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 # Import Dataset
 
@@ -20,91 +21,61 @@ kumpulan_lirik = df["lirik"].tolist()
 
 # Vektorisasi dataset menggunakan TF-IDF
 
-from sklearn.feature_extraction.text import TfidfVectorizer
-
 tfidf_v = TfidfVectorizer()
 vektor_lirik = tfidf_v.fit_transform(kumpulan_lirik)
 
-target = StringVar()
+window = Tk()
+input_text = StringVar()
+window.geometry("480x480")
+window.title("Song Finder")
+
+homepage = Frame(window)
+
+homepage.grid(row=0, column=0, sticky='news')
 
 # Fungsi mencari lagu
-def searchSong(query):
-   query = target.strip.lower()
-   query_vector = tfidf_v.transform[query]
-   cos_sim = cosine_similarity(query_vector, vektor_lirik)
-   sorted_index = np.ragsort(cos_sim)[0][::1]
-   hasil = sorted_index[0]
-   hasil = df[hasil]
-   return hasil
 
-def search():
-   choosen = searchSong()
+def cari_lagu():
+    global input_text
+    query = input_text.get().lower()
+    if query == "":
+        messagebox.showerror("Error", "Input some lyrics!")
+    else:
+        query_vektor = tfidf_v.transform([query])
+        cos_sim = cosine_similarity(query_vektor, vektor_lirik)
+        sorted_index = np.argsort(cos_sim)[0][::-1]
+        index_relevan = []
+        for i in sorted_index:
+        # Memfilter hasil dokumen yang memiliki nilai cosine similarity > 0
+            if cos_sim[0][i] > 0:
+                index_relevan.append([i, cos_sim[0][i]])
+                
+        for i in index_relevan:
+            print("Dokumen {} memiliki cosine similarity sebesar {:.2f}".format(i[0], i[1]))
+        print(f"Dokumen relevan yang dihasilkan sistem yakni : {len(index_relevan)} \n")
 
+        lagu = []
+        for i in index_relevan:
+            x = df.iloc[i[0]]['judul']
+            y = df.iloc[i[0]]['artis']
+            z = df.iloc[i[0]]['lirik']
+            lagu.append([x, y, z])
 
-# Penerimaan Inputan Kueri dari User
+        # Jika lagu tidak ada pada dataset
+        if len(lagu) == 0:
+            messagebox.showinfo("Info", "It doens't exist!")
+        else:
+            hasil = Frame(window)
+            hasil.grid(row=0, column=0, sticky='news')
+            hasil.tkraise()
+            Label(hasil, text="Lagu yang anda cari adalah: ",  font=("Times",20,"bold")).pack()
+            Label(hasil, text=lagu[0][0]).pack()
+            Button(hasil, text="search again", command=lambda:hasil.destroy()).pack()
+            Button(hasil, text="close", command=lambda:window.destroy()).pack()
 
-query = input("Masukkan lirik lagu yang akan dicari : ")
+Label(homepage, text="Song Finder", bg="teal", fg="white", font=("Times",20,"bold")).pack(padx=100, pady=5)
+Entry(homepage, text="Masukkan penggalan lirik",font=("Times",20,"bold"),  textvariable=input_text).pack(padx=100, pady=5)
+Button(homepage, text="Cari", font=("Times",14,"bold"), command=lambda:cari_lagu()).pack(padx=100, pady=5)
 
-# Preprocessing Kueri
-
-query = query.strip().lower()
-
-# Menghitung nilai Cosine Similarity
-
-def search_song(query, vektor):
-  # Vektorisasi kueri
-  query_vector = tfidf_v.transform([query])
-  # Mengembalikan nilai cosine similarity
-  return cosine_similarity(query_vector, vektor)
-
-cos_sim = search_song(query, vektor_lirik)
-
-sorted_index = np.argsort(cos_sim)[0][::-1]
-index_relevan = []
-
-for i in sorted_index:
-    # Memfilter hasil dokumen yang memiliki nilai cosine similarity > 0
-    if cos_sim[0][i] > 0:
-      index_relevan.append([i, cos_sim[0][i]])
-
-# Menampilkan urutan nilai Cosine Similarity Dokumen Relevan (Descending) 
-for i in index_relevan:
-    print("Dokumen {} memiliki cosine similarity sebesar {:.2f}".format(i[0], i[1]))
-
-# Jumlah Dokumen relevan yang dihasilkan Sistem
-print(f"Dokumen relevan yang dihasilkan sistem yakni : {len(index_relevan)} ")
-
-# Inisialisasi Variabel untuk menampung dokumen relevan
-
-hasil = []
-
-# Menyimpan hasil dokumen relevan pada variabel
-
-for i in index_relevan:
-    x = df.iloc[i[0]]['judul']
-    y = df.iloc[i[0]]['artis']
-    z = df.iloc[i[0]]['lirik']
-    hasil.append([x, y, z])
-
-hasil = pd.DataFrame(hasil)
-hasil.columns = ["Judul", "Artis", "Lirik"]
-
-hasil
-
-# Penghitungan Mean Reciprocal Rank (MRR) sebagai evaluasi sistem
-
-# def compute_mrr(rankings, query):
-#     reciprocal_ranks = []
-#     count = 0
-#     for idx in rankings.index:
-#       reciprocal_ranks.append(1/(count+1))
-#       if query in rankings["Lirik"][idx]:
-#           break
-#       count += 1
-#     print(reciprocal_ranks)
-#     print(f"Dokumen yang cocok dengan pencarian anda berada pada dokumen: {count}")
-#     return sum(reciprocal_ranks)/len(reciprocal_ranks)
-
-# hasil_mrr =  compute_mrr(hasil, query);
-
-# print(f"Hasil MRR sistem terhadap kueri {query} adalah : {hasil_mrr}")
+homepage.tkraise()
+window.mainloop()
