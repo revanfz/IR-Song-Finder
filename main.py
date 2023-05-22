ef23688b3f0d8fde48e2d4dcbd386202c49fda7f
Raven <revanfauzi81@gmail.com>
2023-05-22 18:42:32 +0700

    Fix GUI, placeholder entry, styling

--- main.py
+++ main.py
@@ -9,7 +9,8 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 
 # Import Dataset
 
-df = pd.read_csv("https://raw.githubusercontent.com/revanfz/revanfz/main/dataset_lirik.csv", encoding="utf-8")
+df = pd.read_csv(
+    "https://raw.githubusercontent.com/revanfz/revanfz/main/dataset_lirik.csv", encoding="utf-8")
 ''' 
 Preprocessing text
     Casefolding : merubah semua huruf menjadi lowecase (huruf kecil)
@@ -35,6 +36,17 @@ homepage.grid(row=0, column=0, sticky='news')
 
 # Fungsi mencari lagu
 
+
+def click(*args):
+    obj.delete(0, 'end')
+
+
+def leave(*args):
+    obj.delete(0, 'end')
+    obj.insert(0, 'I am placeholder')
+    obj.focus()
+
+
 def cari_lagu():
     global input_text
     query = input_text.get().lower()
@@ -46,13 +58,15 @@ def cari_lagu():
         sorted_index = np.argsort(cos_sim)[0][::-1]
         index_relevan = []
         for i in sorted_index:
-        # Memfilter hasil dokumen yang memiliki nilai cosine similarity > 0
+            # Memfilter hasil dokumen yang memiliki nilai cosine similarity > 0
             if cos_sim[0][i] > 0:
                 index_relevan.append([i, cos_sim[0][i]])
-                
+
         for i in index_relevan:
-            print("Dokumen {} memiliki cosine similarity sebesar {:.2f}".format(i[0], i[1]))
-        print(f"Dokumen relevan yang dihasilkan sistem yakni : {len(index_relevan)} \n")
+            print(
+                "Dokumen {} memiliki cosine similarity sebesar {:.2f}".format(i[0], i[1]))
+        print(
+            f"Dokumen relevan yang dihasilkan sistem yakni : {len(index_relevan)} \n")
 
         lagu = []
         for i in index_relevan:
@@ -68,14 +82,27 @@ def cari_lagu():
             hasil = Frame(window)
             hasil.grid(row=0, column=0, sticky='news')
             hasil.tkraise()
-            Label(hasil, bg="teal", fg="white", text="Song Title : ",  font=("Times",20,"bold")).pack(pady=20)
+            Label(hasil, bg="teal", fg="white", text="Song Title : ",
+                  font=("Times", 20, "bold")).pack(pady=20)
             Label(hasil, font=("Times", 18), text=lagu[0][0]).pack()
-            Button(hasil, font=("Times",14,"bold"), text="Search again", command=lambda:hasil.destroy()).pack(pady=10)
-            Button(hasil, font=("Times",14,"bold"), text="Close", command=lambda:window.destroy()).pack()
+            Button(hasil, font=("Times", 14, "bold"), text="Search again",
+                   command=lambda: hasil.destroy()).pack(pady=10)
+            Button(hasil, font=("Times", 14, "bold"), text="Close",
+                   command=lambda: window.destroy()).pack()
 
-Label(homepage, text="Song Finder", bg="teal", fg="white", font=("Times",20,"bold")).pack(padx=100, pady=5)
-Entry(homepage, text="Masukkan penggalan lirik",font=("Times",20,"bold"),  textvariable=input_text).pack(padx=100, pady=5)
-Button(homepage, text="Cari", font=("Times",14,"bold"), command=lambda:cari_lagu()).pack(padx=100, pady=5)
+
+Label(homepage, text="Song Finder", bg="teal", fg="white",
+      font=("Times", 20, "bold")).pack(padx=100, pady=5)
+obj = Entry(homepage, text="Masukkan penggalan lirik", font=(
+    "Times", 20, "bold"),  textvariable=input_text)
+obj.pack(padx=100, pady=5)
+Button(homepage, text="Cari", font=("Times", 14, "bold"),
+       command=lambda: cari_lagu()).pack(padx=100, pady=5)
+
+obj.insert(0, 'Enter Text:- ')
+obj.pack(pady=10)
+obj.bind("<Button-1>", click)
+# obj.bind("<Leave>", leave)
 
 homepage.tkraise()
 window.mainloop()
